e3c63a16cbe6ac46c698a30f90138c965a4f0197
Shiva Rakshith <rakshiths@sanketika.in>
2021-04-12 01:21:23 +0530

    committing latest changes.

--- user-service/app/controllers/HomeController.scala
+++ user-service/app/controllers/HomeController.scala
@@ -11,14 +11,16 @@ import scala.concurrent.{ExecutionContext, Future}
 import scala.language.postfixOps
 
 @Singleton
-class HomeController @Inject()(@Named("userActor") userActor: ActorRef, cc: ControllerComponents)(implicit exec: ExecutionContext) extends AbstractController(cc){
+class HomeController @Inject()(@Named("timeActor") timeActor: ActorRef, cc: ControllerComponents)(implicit exec: ExecutionContext) extends AbstractController(cc){
 
   def home : Action[AnyContent] = Action { implicit request =>
     Ok("Home Page")
   }
 
-  def getTime(timeZone : String) : Action[AnyContent] = Action.async { implicit request =>
-   val result : Future[String] = ask(userActor,getTimeZone(timeZone))(5 second).mapTo[String]
+  def getTime() : Action[AnyContent] = Action.async { implicit request =>
+    val body = request.body.asJson.get
+    val timezone: String = (body \ "timezone").as[String]
+    val result : Future[String] = ask(timeActor,getTimeZone(timezone))(5 second).mapTo[String]
     result.map(response => Ok(response).withHeaders(CONTENT_TYPE -> "application/json"))
   }
 
